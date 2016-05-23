e4e1fc18579b70ed1a89f2cff3ba054b1a96ed11
cantcool <vyachaslau@gmail.com>
2016-05-23 20:16:50 +0300

    Initial webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  entry: "./home",
+  output: {
+    filename: "build.js",
+    library: "home"
+  },
+
+  watch: true,
+
+  watchOptions: {
+    aggregateTimeout: 150
+  }
+};
